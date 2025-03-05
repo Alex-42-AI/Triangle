e123a7daa41ae1b7585a883bfa1b8c838a5e8c52
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-03-05 12:06:31 +0200

    Update Functions.py

--- Triangle/Functions.py
+++ Triangle/Functions.py
@@ -1,5 +1,8 @@
 from math import sqrt, sin, cos, asin, acos, pi
 
+__all__ = ["legit_angle", "get_side_from_sin_theorem", "get_side_from_2_sides", "get_third_angle", "sqrt", "sin",
+           "get_angle_from_3_sides", "get_angle_from_3_sides", "get_angle_from_height_and_side", "get_height",
+           "get_surface", "get_median", "get_bisector", "get_outer_radius", "get_inner_radius", "pi", "cos"]
 
 def legit_angle(angle, given=0):
     return 0 < angle < pi - given
@@ -30,7 +33,8 @@ def get_height(surface, side):
 
 
 def get_surface(side_1, side_2, side_3):
-    return sqrt((side_1 + side_2 + side_3) * (side_2 + side_3 - side_1) * (side_1 - side_2 + side_3) * (side_1 + side_2 - side_3)) / 4
+    return sqrt((side_1 + side_2 + side_3) * (side_2 + side_3 - side_1) * (side_1 - side_2 + side_3) * (
+            side_1 + side_2 - side_3)) / 4
 
 
 def get_median(opposite, side_1, side_2):
